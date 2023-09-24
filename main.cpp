1d8a46ef40cfe6bdf11357fb161f7a3ccf73c280
Hummingbird <stphpedersen@gmail.com>
2023-09-24 21:38:33 +0200

    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fatal heart
+//
+//  Created by Persephone on 24/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
